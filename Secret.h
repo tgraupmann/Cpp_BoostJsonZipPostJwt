cfc57b8de38175ecafbd60b7f167c84617d27280
Timothy Graupmann <tim.graupmann@razer.com>
2020-07-16 11:05:25 -0700

    Added jsoncpp

---
+++ Secret.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <string>
+
+const std::string VALUE_BACKEND_SECRET = "TWITCH BACKEND SECRET";
--- main.cpp
+++ main.cpp
@@ -17,9 +17,13 @@
 #define NOMINMAX
 #include <Windows.h>
 
+// Ref: https://github.com/Thalhammer/jwt-cpp
 #undef max
 #undef min 
 #include <jwt-cpp/jwt.h>
+
+#include "Secret.h"
+#include <json/json.h>
 
 using namespace std;
 
@@ -63,6 +67,47 @@ string DoBase64Encode(const string& text)
     return os.str();
 }
 
+void DoJWT(string userId)
+{
+    string secret = VALUE_BACKEND_SECRET;
+    int mod4 = secret.size() % 4;
+    if (mod4 > 0)
+    {
+        secret += string('=', 4 - mod4);
+    }
+
+
+    Json::FastWriter fastWriter;
+    
+
+    Json::Value jsonSubject;
+    jsonSubject["user_id"] = userId;
+    jsonSubject["role"] = "external";
+    string strSubject = fastWriter.write(jsonSubject);
+
+    auto future = std::chrono::system_clock::now() + std::chrono::hours{ 48 };
+    auto exp = std::chrono::duration_cast<std::chrono::microseconds>(future.time_since_epoch()).count();
+
+    Json::Value jsonPayload;
+    jsonPayload["exp"] = exp;
+    jsonPayload["channel_id"] = userId;
+    jsonPayload["user_id"] = userId;
+    jsonPayload["role"] = "external";
+    jsonPayload["pubsub_perms"]["send"][0] = "*";
+    string strJson = fastWriter.write(jsonPayload);
+
+    auto token = jwt::create()
+        .set_issuer("Twitch")
+        .set_issued_at(std::chrono::system_clock::now())
+        .set_expires_at(future)
+        .set_type("JWS")
+        .set_subject(strSubject)
+        .set_payload_claim("sample", jwt::claim(strJson))
+        .sign(jwt::algorithm::hs256{ secret });
+
+    cout << "Token: " << token.c_str() << endl;
+}
+
 int main()
 {
     string uncompressed = "Hello World! Hello Hello Hello Hello Hello";
@@ -76,4 +121,6 @@ int main()
     string base64 = DoBase64Encode(compressed);
     cout << "Base64 Size: " << base64.size() << endl;
     cout << "Base64: " << base64 << endl;
+
+    DoJWT("12345");
 }
