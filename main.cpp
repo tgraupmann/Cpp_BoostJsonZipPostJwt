13e66f4d3baaf08d3e6c7e029c1992a1224fdbac
Timothy Graupmann <tim.graupmann@razer.com>
2020-07-17 11:30:18 -0700

    Print response with wcout

--- main.cpp
+++ main.cpp
@@ -20,7 +20,6 @@
 #include <boost/archive/iterators/ostream_iterator.hpp>
 
 #include <cpprest/http_client.h>
-#include <cpprest/filestream.h>
 
 #include <Windows.h>
 
@@ -123,44 +122,28 @@ string DoJWT(string userId)
 
 void DoPost()
 {
-    using ostream = Concurrency::streams::ostream;
-    using fstream = Concurrency::streams::fstream;
-
-    auto fileStream = std::make_shared<ostream>();
-
-    // Open stream to output file.
-    pplx::task<void> requestTask = fstream::open_ostream(U("results.html")).then([=](ostream outFile)
-    {
-        *fileStream = outFile;
-
-        // Create http_client to send the request.
-        http_client client(U("http://www.bing.com/"));
-
-        // Build request URI and start the request.
-        uri_builder builder(U("/search"));
-        builder.append_query(U("q"), U("cpprestsdk github"));
-        return client.request(methods::GET, builder.to_string());
-    })
-
-    // Handle response headers arriving.
-    .then([=](http_response response)
-    {
-        printf("Received response status code:%u\n", response.status_code());
-
-        // Write response body into the file.
-        return response.body().read_to_end(fileStream->streambuf());
-    })
-
-    // Close the file stream.
-    .then([=](size_t)
-    {
-        return fileStream->close();
-    });
-
     // Wait for all the outstanding I/O to complete and handle any exceptions
     try
     {
-        requestTask.wait();
+        // Create http_client to send the request.
+        http_client client(U("http://tagenigma.com/"));
+
+        // Build request URI and start the request.
+        uri_builder builder(U("/post"));
+        builder.append_query(U("q"), U("cpprestsdk github"));
+        client.request(methods::GET, builder.to_string())
+
+        // Handle response headers arriving.
+        .then([=](http_response response)
+        {
+            printf("Received response status code:%u\n", response.status_code());
+            return response.extract_string(true);
+        })
+        .then([=](wstring content)
+        {
+            wcout << L"Content: " << content << endl;
+        })
+        .wait();
     }
     catch (const std::exception& e)
     {
