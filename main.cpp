448d36c93a54af638c6aa72acb1ae3223f2301ff
Timothy Graupmann <tim.graupmann@razer.com>
2020-11-24 17:46:17 -0800

    CppRest has a WebSocket client

--- main.cpp
+++ main.cpp
@@ -21,6 +21,10 @@
 
 #include <cpprest/http_client.h>
 
+#include <cpprest/ws_client.h>
+using namespace web;
+using namespace web::websockets::client;
+
 #include <Windows.h>
 
 // Ref: https://github.com/Thalhammer/jwt-cpp
@@ -39,6 +43,44 @@ using namespace web::http::client;          // HTTP client features
 using namespace concurrency::streams;       // Asynchronous streams
 
 #include <tchar.h>
+
+// ref: https://github.com/microsoft/cpprestsdk/wiki/Web-Socket
+void DoWSS()
+{
+    websocket_client client;
+    std::string body_str("{\"action\":\"sendmessage\", \"data\":\"hello C++\"}");
+
+    try
+    {
+        client.connect(U("wss://WEB_SOCKET_ENDPOINT")).wait();
+        auto receive_task = client.receive().then([body_str](websocket_incoming_message ret_msg) {
+            //VERIFY_ARE_EQUAL(ret_msg.length(), body_str.length());
+            auto ret_str = ret_msg.extract_string().get();
+
+            //VERIFY_ARE_EQUAL(body_str.compare(ret_str), 0);
+            //VERIFY_ARE_EQUAL(ret_msg.message_type(), websocket_message_type::text_message);
+            });
+
+        websocket_outgoing_message msg;
+        msg.set_utf8_message(body_str);
+        client.send(msg).wait();
+
+        receive_task.wait();
+        client.close().wait();
+    }
+    catch (const websocket_exception& e)
+    {
+        /*
+        if (is_timeout(e.what()))
+        {
+            // Since this test depends on an outside server sometimes it sporadically can fail due to timeouts
+            // especially on our build machines.
+            return;
+        }
+        */
+        cerr << "Fix your WSS endpoint!" << endl;
+    }
+}
 
 string DoCompression(const string& data)
 {
@@ -180,6 +222,9 @@ void DoPost()
 
 int main()
 {
+    cout << "Testing C++ WebSocket Client..." << endl;
+    DoWSS();
+
     string uncompressed = "Hello World! Hello Hello Hello Hello Hello";
     cout << "Uncompressed Size: " << uncompressed.size() << endl;
     cout << "Uncompressed: " << uncompressed << endl;
